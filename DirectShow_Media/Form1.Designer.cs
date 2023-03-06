7361988e1df48f2f3b057b6411d8bdab9b8c6650
Hyun <hickee32@gmail.com>
2023-03-06 21:55:35 +0900

    DirectShow

---
+++ DirectShow_Media/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Test_Media {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.time_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 384);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "재생";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "정지";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.time_label.Location = new System.Drawing.Point(560, 412);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(98, 21);
+            this.time_label.TabIndex = 3;
+            this.time_label.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "재생";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label time_label;
+    }
+}
---
+++ DirectShow_Media/Form1.cs
@@ -0,0 +1,85 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test_Media {
+
+    public partial class Form1 : Form {
+
+        string filePath = "C:\\Users\\bluep\\OneDrive\\바탕 화면\\Test_Media\\video\\sample.wmv";
+
+        IGraphBuilder pGrahpBuilder = null;
+        IMediaControl pMediaControl = null;
+
+        IMediaEvent pMediaEvent = null;
+        EventCode eventCode;
+
+        IVideoWindow pVideoWindow= null;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+
+            if (button1.Text == "일시정지") {
+                pMediaControl.Pause();
+
+                button1.Text = "재생";
+            }
+            else { 
+
+                Test();
+                button1.Text = "일시정지";
+            }
+         }
+
+        private void Test() {
+            this.Text = filePath;
+            pGrahpBuilder = (IGraphBuilder)new FilterGraph();
+
+            pMediaControl = (IMediaControl)pGrahpBuilder;
+
+            pMediaEvent = (IMediaEvent)pGrahpBuilder;
+
+            pVideoWindow = (IVideoWindow)pGrahpBuilder;
+
+            pMediaControl.RenderFile(filePath);
+
+            //재생
+            pVideoWindow.put_Owner(panel1.Handle);
+            pVideoWindow.put_WindowStyle(WindowStyle.Child | WindowStyle.ClipSiblings);
+            Rectangle rect = panel1.ClientRectangle;
+            pVideoWindow.SetWindowPosition(0,0, rect.Right, rect.Bottom);
+            pMediaControl.Run();
+            Console.WriteLine(pMediaControl.Run());
+            
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            Console.WriteLine(pMediaControl.Run());
+
+            Marshal.ReleaseComObject(pGrahpBuilder);
+            pGrahpBuilder = null;
+            Marshal.ReleaseComObject(pMediaControl);
+            pMediaControl = null;
+            Marshal.ReleaseComObject(pMediaEvent);
+            pMediaEvent = null;
+            Marshal.ReleaseComObject(pVideoWindow);
+            pVideoWindow = null;
+        }
+    }
+}
