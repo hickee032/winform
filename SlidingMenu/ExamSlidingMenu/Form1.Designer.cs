9735f555a46c7b1f69540340c7cabb5a7dde0e3b
Hyun <hickee32@gmail.com>
2023-03-06 14:04:02 +0900

    SlidingMenu

---
+++ SlidingMenu/ExamSlidingMenu/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ExamSlidingMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.checkBoxHide = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerSliding = new System.Windows.Forms.Timer(this.components);
+            this.panelSideMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelSideMenu.Controls.Add(this.checkBoxHide);
+            this.panelSideMenu.Controls.Add(this.button2);
+            this.panelSideMenu.Controls.Add(this.button1);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(200, 450);
+            this.panelSideMenu.TabIndex = 0;
+            // 
+            // checkBoxHide
+            // 
+            this.checkBoxHide.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBoxHide.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkBoxHide.FlatAppearance.BorderSize = 0;
+            this.checkBoxHide.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.checkBoxHide.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.checkBoxHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxHide.ForeColor = System.Drawing.Color.White;
+            this.checkBoxHide.Location = new System.Drawing.Point(0, 400);
+            this.checkBoxHide.Name = "checkBoxHide";
+            this.checkBoxHide.Size = new System.Drawing.Size(200, 50);
+            this.checkBoxHide.TabIndex = 2;
+            this.checkBoxHide.Text = "<";
+            this.checkBoxHide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxHide.UseVisualStyleBackColor = true;
+            this.checkBoxHide.CheckedChanged += new System.EventHandler(this.checkBoxHide_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "메뉴2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "메뉴1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timerSliding
+            // 
+            this.timerSliding.Interval = 10;
+            this.timerSliding.Tick += new System.EventHandler(this.timerSliding_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelSideMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelSideMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxHide;
+        private System.Windows.Forms.Timer timerSliding;
+    }
+}
+
---
+++ SlidingMenu/ExamSlidingMenu/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamSlidingMenu
+{
+    public partial class Form1 : Form
+    {
+        //슬라이딩 메뉴의 최대, 최소 폭 크기
+        const int MAX_SLIDING_WIDTH = 200;
+        const int MIN_SLIDING_WIDTH = 50;
+        //슬라이딩 메뉴가 보이는/접히는 속도 조절
+        const int STEP_SLIDING = 10;
+        //최초 슬라이딩 메뉴 크기
+        int _posSliding = 200;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxHide_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxHide.Checked == true)
+            {
+                //슬라이딩 메뉴가 접혔을 때, 메뉴 버튼의 표시
+                button1.Text = "1";
+                button2.Text = "2";
+                checkBoxHide.Text = ">";
+            }
+            else
+            {
+                //슬라이딩 메뉴가 보였을 때, 메뉴 버튼의 표시
+                button1.Text = "버튼1";
+                button1.BackgroundImage = null;
+                button2.Text = "버튼2";
+                button2.BackgroundImage = null;
+                checkBoxHide.Text = "<";
+            }
+
+            //타이머 시작
+            timerSliding.Start();
+        }
+
+        private void timerSliding_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxHide.Checked == true)
+            {
+                //슬라이딩 메뉴를 숨기는 동작
+                _posSliding -= STEP_SLIDING;
+                if (_posSliding <= MIN_SLIDING_WIDTH)
+                    timerSliding.Stop();
+            }
+            else
+            {
+                //슬라이딩 메뉴를 보이는 동작
+                _posSliding += STEP_SLIDING;
+                if (_posSliding >= MAX_SLIDING_WIDTH)
+                    timerSliding.Stop();
+            }
+
+            panelSideMenu.Width = _posSliding;
+        }
+    }
+}
